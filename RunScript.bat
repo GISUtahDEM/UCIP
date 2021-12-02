089e04858bba0e5e7f01385a05e7d9c45c92d03f
GISUtahDEM <79233316+GISUtahDEM@users.noreply.github.com>
2021-12-02 07:51:27 -0700

    Rename RunScript.bat to RunScript_UCIPLayers.bat

--- RunScript.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\propy.bat" "C:\Users\jsurkis\Documents\TEST_UCIP.py"
---
+++ RunScript_UCIPLayers.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\propy.bat" "C:\Users\jsurkis\Documents\TEST_UCIP.py"
